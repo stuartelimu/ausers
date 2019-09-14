1208959849207d1e8a207e076dd1c56e5cab7cf5
ElimuStuart <stuartelimu@gmail.com>
2019-09-14 06:57:04 +0300

    create dashboard view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
 
-# Create your views here.
+class DashboardView(LoginRequiredMixin, TemplateView):
+    template_name = "dashboard.html"
