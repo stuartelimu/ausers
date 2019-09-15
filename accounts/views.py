9c546fdf623a8204c1613dfaefcb2b7eaec4b698
ElimuStuart <stuartelimu@gmail.com>
2019-09-15 07:56:54 +0300

    fix logout bug add static files for log in template

--- accounts/views.py
+++ accounts/views.py
@@ -2,4 +2,4 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 class DashboardView(LoginRequiredMixin, TemplateView):
-    template_name = "dashboard.html"
+    template_name = "accounts/dashboard.html"
